f17fb77c4d246c04661ff0421a7f2aa6032a4d23
Cyber-SoftWorker <yudrickmartin@gmail.com>
2026-06-08 15:22:11 +0200

    Add Docker implementation for HRM application

--- HR.LeaveManagement.BlazorUI/Program.cs
+++ HR.LeaveManagement.BlazorUI/Program.cs
@@ -17,7 +17,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddTransient<JwtAuthorizationMessageHandler>();
-builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7112"))
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("http://localhost:8080"))
     .AddHttpMessageHandler<JwtAuthorizationMessageHandler>(); ;
 
 builder.Services.AddBlazoredToast();
